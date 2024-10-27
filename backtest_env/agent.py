eb1b05d13781908d435b991943e99800b5678c7a
hanhvn <vonhathanh@gmail.com>
2024-10-27 19:27:15 +0700

    more pseudo codes

--- backtest_env/agent.py
+++ backtest_env/agent.py
@@ -1,3 +1,4 @@
+from backtest_env.order_dispatcher import OrderDispatcher
 from backtest_env.env import Env
 from backtest_env.strategies.strategy import Strategy
 
@@ -7,14 +8,27 @@ class Agent:
     # we'll simulate websocket event using a queue, API call by a middleware (OrderDispatcher)
     def __init__(self, env: Env, params: dict):
         self.env = env
+        # env load the requested data from agent params
         self.env.load_data(params["symbols"], params["timeframes"])
+        # register agent to the env
         self.env.add_agent(self)
+        # init agent's strategy
         self.strategy = Strategy(params["strategy"])
+        # agent also has a queue to process event from the engine
+        self.queue = None
 
     def run(self):
+        self.process_events()
         # collect new information from the environment and send them to the strategy
+        # data can be candlestick prices, agent's order history...
         data = self.get_data()
-        self.strategy.run(data)
+        # strategy will determine whether there is a trading opportunity or not
+        orders = self.strategy.run(data)
+        # validate the orders in agent's perspective
+        # orders might conflict with it current positions
+        orders = self.validate_orders(orders)
+        # submit orders using the dispatcher
+        OrderDispatcher.dispatch(orders)
 
     def report(self):
         pass
--- backtest_env/env.py
+++ backtest_env/env.py
@@ -3,19 +3,23 @@ from backtest_env.agent import Agent
 
 class Env:
     def __init__(self):
+        # list of active agents
         self.agents = []
+        # message queue to process events/requests from agents
         self.queue = None
+        # database backend to store order history, positions
+        self.db = None
 
-    def step(self):
-        # move forward one candle
+    def step(self) -> bool:
+        # moves forward one candle in the predefined time range
+        # return None if data is not available
         pass
 
     def set_backtest_range(self, start_time: int, end_time: int = 0):
         pass
 
     def run(self):
-        while True:
-            self.step()
+        while self.step():
             self.process_events()
             for agent in self.agents:
                 agent.run()
