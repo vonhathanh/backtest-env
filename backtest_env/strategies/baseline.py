17887c02213412d8e1cb67e5cb96ee3188a6a94a
hanhvn <vonhathanh@gmail.com>
2024-10-30 11:36:17 +0700

    add comments

--- backtest_env/strategies/baseline.py
+++ backtest_env/strategies/baseline.py
@@ -3,13 +3,14 @@ from backtest_env.utils import market_order
 from backtest_env.constants import *
 
 class Baseline(Strategy):
-    # base class for all strategies
-    # contains logics to enter/exit trades
+    # base class for strategy
+    # contains logics to enter/exit trades, close positions
     def __init__(self, params):
         super().__init__()
         self.params = params
 
 
     def run(self, data: dict) -> list:
+        # order can be normal buy/sell order or a special action such as cancel orders, close positions
         order = market_order(self.params["symbol"], BUY, 0.0, 1.0)
         return [order]
