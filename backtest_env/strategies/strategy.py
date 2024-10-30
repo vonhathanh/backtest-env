1c7df59f8bfe47c2a31553de395c559c5df29412
hanhvn <vonhathanh@gmail.com>
2024-10-30 11:36:54 +0700

    add comments

--- backtest_env/strategies/strategy.py
+++ backtest_env/strategies/strategy.py
@@ -4,7 +4,7 @@ T = TypeVar("T", bound="Strategy")
 
 class Strategy:
     # base class for all strategies
-    # contains logics to enter/exit trades
+    # contains logics to enter/exit trades, close positions
     def __init__(self, params: dict = None):
         pass
 
@@ -12,6 +12,7 @@ class Strategy:
         # validate agent's state: orders, positions,...
         # inspect new input data: prices, indicators,...
         # create orders if necessary
+        # order can be normal buy/sell order or a special action such as cancel orders, close positions
         pass
 
     @classmethod
