3fce73ba2fff008d4ef8724445bd8e3401c3b9c3
hanhvn <vonhathanh@gmail.com>
2024-11-05 11:38:46 +0700

    design trend follower

--- backtest_env/strategies/trend_follower.py
+++ backtest_env/strategies/trend_follower.py
@@ -1,3 +1,4 @@
+from backtest_env.backend import Backend
 from backtest_env.strategies import Strategy
 
 
@@ -12,5 +13,27 @@ class TrendFollower(Strategy):
     6) Close all positions/orders at the end of the next candle
     7) Test this one with highly volatile symbols
     8) Need an indicator/measure to know which pair is volatile
+    This strategy only work when there is little wick in the candle body: engulfing,
+    How do we reduce the risk when the market is not giving us the right candle?
+    - Trail stoploss
+    - 
     """
-    pass
+    def __init__(self, params):
+        super().__init__(params)
+
+    def run(self, backend: Backend):
+        # get pending orders and positions
+        pending_orders = backend.get_pending_orders()
+        positions = backend.get_positions()
+        # close all orders and positions
+        backend.cancel_all_pending_orders()
+        backend.close_all_positions()
+        # start new grid
+        # get latest close price
+        price = backend.get_prices()
+        # get statistics like grid interval, size from price history
+        interval = calculate_grid_interval()
+        grid_size = calculate_grid_size()
+        # place grid orders
+        place_grid_orders(price, BUY, grid_size, interval, self.params["quantity"])
+        place_sell_orders(price, BUY, grid_size, interval, self.params["quantity"])
