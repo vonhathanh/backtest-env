659674e92a17c5756fd192bd3e5e307fe3631364
hanhvn <vonhathanh@gmail.com>
2024-11-05 09:35:09 +0700

    add designs for two new stratgies

---
+++ backtest_env/strategies/buy_low_sell_high.py
@@ -0,0 +1,7 @@
+class BuyLowSellHigh:
+    """
+    Idea: continuously dca when we buy/sell
+    Place buy orders below open price and sell order above the open price
+    Take profit partially
+    """
+    pass
---
+++ backtest_env/strategies/trend_follower.py
@@ -0,0 +1,13 @@
+class TrendFollower:
+    """
+    1) Open both long and short at daily candle close price
+    2) Place limit long orders at the upper side of the entry price.
+    3) Place limit short orders at the lower side of the entry price.
+       Each long/short order has equal price difference with its predecessor/successor
+    4) Price difference is the average 1h-4h candle's length
+    5) To avoid complexity, we'll place 20 orders at both sides
+    6) Close all positions/orders at the end of the next candle
+    7) Test this one with highly volatile symbols
+    8) Need an indicator/measure to know which pair is volatile
+    """
+    pass
