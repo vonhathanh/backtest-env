c4b6522314b06b80dcc371da0849a74efb1d1173
hanhvn <vonhathanh@gmail.com>
2024-12-04 21:53:12 +0700

    delete unused strategy

--- backtest_env/strategies/buy_low_sell_high.py
+++
@@ -1,7 +0,0 @@
-class BuyLowSellHigh:
-    """
-    Idea: continuously dca when we buy/sell
-    Place buy orders below open price and sell order above the open price
-    Take profit partially
-    """
-    pass
