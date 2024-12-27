09b79131961f87d68cb437692b8e7a18ae35f0b5
hanhvn <vonhathanh@gmail.com>
2024-12-27 15:05:18 +0700

    delete agent, env, order dispatcher

--- backtest_env/order_dispatcher.py
+++
@@ -1,6 +0,0 @@
-class OrderDispatcher:
-    # send orders to the environment/engine
-    # normally, we use GET request to submit order but in simulation we don't have to do that
-    @staticmethod
-    def dispatch(orders: list, target):
-        target.add_orders(orders)
