eab785fb5ab69597190cfec59198964adaa90604
hanhvn <vonhathanh@gmail.com>
2025-02-02 21:28:00 +0700

    add /backtest api, shutdown procedure and test background process

--- backtest_env/app.py
+++ backtest_env/app.py
@@ -1,25 +1,54 @@
-import multiprocessing
+import time
+from multiprocessing import Process, Queue
 
+from pydantic import BaseModel
 from fastapi import FastAPI, WebSocket
 
-event_queue = multiprocessing.Queue()
+event_queue = Queue()
 
 ws_clients: set[WebSocket] = set()
+
+processes: list[Process] = []
 
 app = FastAPI()
 
 
+class BacktestParam(BaseModel):
+    id: int
+
 @app.get("/")
-async def index():
-    return {"status": "OK"}
+def index():
+    return {"msg": "OK"}
+
+@app.on_event("shutdown")
+def shutdown():
+    for client in ws_clients:
+        client.close()
+
+    for process in processes:
+        process.join()
 
 
 @app.websocket("/ws")
 async def websocket_connected(websocket: WebSocket):
     await websocket.accept()
     await websocket.send_json({"msg": "hello websocket"})
+
     ws_clients.add(websocket)
 
+@app.post("/backtest")
+def backtest(args: BacktestParam):
+    backtest_process = Process(target=f, args=(args, ))
+    backtest_process.start()
+
+    processes.append(backtest_process)
+
+    return {"msg": "OK"}
+
+
+def f(args):
+    time.sleep(5)
+    print("f completed")
 
 async def send_message_to_websocket_client():
     event = event_queue.get()
