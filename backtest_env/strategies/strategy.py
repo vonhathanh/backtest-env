3ba5c5f46be7512b1c1c7f6d13517e2f188337f2
hanhvn <vonhathanh@gmail.com>
2025-02-03 16:58:27 +0700

    readd method to create Strategy from params

--- backtest_env/strategies/strategy.py
+++ backtest_env/strategies/strategy.py
@@ -1,7 +1,11 @@
+from typing import TypeVar, Type
+
 from backtest_env.backend import Backend
 from abc import ABC, abstractmethod
 
 from backtest_env.price import PriceData
+
+T = TypeVar("T", bound="Strategy")
 
 
 class Strategy(ABC):
@@ -12,7 +16,6 @@ class Strategy(ABC):
         self.data = PriceData(params)
         self.backend = Backend(params["init_balance"], self.data)
 
-
     @abstractmethod
     def run(self):
         # strategy will: validate system's state: orders, positions,...
@@ -20,3 +23,7 @@ class Strategy(ABC):
         # create orders if necessary
         # order can be normal buy/sell order or a special action such as cancel orders, close positions
         pass
+
+    @classmethod
+    def from_cfg(cls: Type[T], params):
+        return cls(params)
