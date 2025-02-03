47d66984e773e22d23698593cbd399cf3ee93dd5
hanhvn <vonhathanh@gmail.com>
2025-02-03 19:13:51 +0700

    add unit test for convert_time_to_nanosecond

---
+++ backtest_env/tests/test_utils.py
@@ -0,0 +1,9 @@
+from backtest_env.utils import convert_time_to_nanosecond
+
+def test_convert_time_to_nanosecond():
+    input_dates = ["01/02/2024", "02-01-2024"]
+    date_formats = ["%M/%d/%Y", "%d-%M-%Y"]
+    expected_timestamps = [1704128460000, 1704128460000]
+
+    for i in range(len(input_dates)):
+        assert convert_time_to_nanosecond(input_dates[i], date_formats[i]) == expected_timestamps[i]
