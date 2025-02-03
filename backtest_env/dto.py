b83ea27cd4a3eb78bd4be1bf5d03910393128226
hanhvn <vonhathanh@gmail.com>
2025-02-03 16:47:53 +0700

    add BacktestParam dto

---
+++ backtest_env/dto.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class BacktestParam(BaseModel):
+    test_id: int
+    strategies: list[str]
+    initial_balance: float
+    symbol: str
+    timeframe: str
+    start_time: str
