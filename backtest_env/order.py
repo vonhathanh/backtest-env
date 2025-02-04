4d5d5135284a1eed4b697adc74d3064c7360a706
hanhvn <vonhathanh@gmail.com>
2025-02-04 16:37:07 +0700

    add OrderType enum

--- backtest_env/order.py
+++ backtest_env/order.py
@@ -1,13 +1,20 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import Optional
 
+
+class OrderType(Enum):
+    Market = 1,
+    Limit = 2,
+    TakeProfit = 3,
+    Stoploss= 4
 
 @dataclass
 class Order:
     price: Optional[float]
     symbol: str
     side: str
-    type: str
+    type: OrderType
     positionSide: Optional[str]
     quantity: float
     id: str
