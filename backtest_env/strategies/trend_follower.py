ad1b3762c90c0d803e037c9295b2e081a640f61e
hanhvn <vonhathanh@gmail.com>
2025-02-08 06:49:43 +0700

    refactor response

--- backtest_env/strategies/trend_follower.py
+++ backtest_env/strategies/trend_follower.py
@@ -44,7 +44,7 @@ class TrendFollower(Strategy):
 
     @classmethod
     def get_required_params(cls):
-        return {"grid_size": "number", "order_size": "number"}
+        return {"Grid Size": "number", "Order Size": "number"}
 
     def run(self):
         while self.data.step():
