0a860ad755a8168d7d7001d2aab6cc28de1ea13c
hanhvn <vonhathanh@gmail.com>
2025-02-08 16:40:55 +0700

    apply clean code principle to trend_follower.py

--- backtest_env/strategies/trend_follower.py
+++ backtest_env/strategies/trend_follower.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import backtest_env.utils as utils
+from backtest_env.dto import TrendFollowerParam
 from backtest_env.order import OrderType, Order
 from backtest_env.strategies import Strategy
 from backtest_env.constants import BUY, SELL
@@ -21,95 +22,97 @@ class TrendFollower(Strategy):
     How do we reduce the risk when the market is not giving us the right candle?
     - Trail stoploss
     """
-    def __init__(self, params):
+    def __init__(self, params: TrendFollowerParam):
         super().__init__(params)
-        self.interval = 0.005
-        self.grid_size = params["grid_size"]
-        self.order_size = params["order_size"]
-        self.symbol = params["symbol"]
-        self.min_num_daily_candles = params["min_num_daily_candles"]
-        # interval = daily_change/step
-        self.step = params["step"]
-        # three ways to calculate self.interval
-        # 1. Hard coded
-        # 2. Add daily candle data and inference the interval from that
-        # 3. Store daily candles in self and derive the interval
-        # we'll go with 3rd method, is complicated, but works in both real and simulated env
-        self.daily_candles = []
-        # parameters of daily candle
-        self.high = 0.0
-        self.low = 2 << 64 # init low to 2^64
-        self.open = 0.0
-        self.close = 0.0
+        self.symbol = params.symbol
+        # how many grid orders will be placed
+        self.grid_size = params.grid_size
+
+        self.order_size = params.order_size
+
+        # the difference in percentage of the next grid order compared to current order
+        # example, current grid order: buy btc at 1000$, step_size = 0.01, so the next order will be placed at
+        # 1000*(1+0.01) = 1010$
+        self.step_size = 0.01
+        # used to calculate step_size dynamically, step_size = daily average change / interval
+        # usually will be around [4, 8] depends on symbol and user's preference
+        self.interval = params.interval
+
+        # number of 1d candles being stored to calculate daily average change
+        self.candle_cache_size = params.candle_cache_size
+        self.candles = []
+        # these values will be used to form daily candle based on smaller candles data
+        self.open, self.high, self.low, self.close = 0.0, 0.0, np.inf, 0.0
 
     @classmethod
     def get_required_params(cls):
         return {"Grid Size": "number", "Order Size": "number"}
 
-    def run(self):
-        while self.data.step():
-            self.order_manager.process_orders()
-            self.update_grid_interval()
-            self.update()
-
     def update(self):
+        self.update_statistic()
+        self.order_manager.process_orders()
+
         if self.is_episode_end():
             # close all orders and positions
             self.order_manager.cancel_all_orders()
             self.position.close()
 
         # only start the strategy when we've collected enough daily candles
-        if len(self.daily_candles) >= self.min_num_daily_candles:
-            # start new grid or update the current grid
+        if len(self.candles) >= self.candle_cache_size:
             self.update_grid()
 
     def is_episode_end(self) -> bool:
         # check if current candle is the first candle in the day (open time = 00:00:00 AM GMT)
-        price = self.data.get_current_price()
+        # time is in millisecond, so we use 86_400_000
         # TODO: check if all daily candles start at 00:00::00 UTC
-        # time is in millisecond so we use 86_400_000
+        price = self.data.get_current_price()
         return price.open_time % 86_400_000 == 0 or self.data.idx == 0
 
-    def update_grid_interval(self):
-        """
-        incharge of add new daily candle and calculate average price change based on those candles
-        """
-        candle = self.data.get_current_price()
+    def update_statistic(self):
+        self.update_candle_cache()
+        # calculate step_size if we've gathered enough candles
+        if len(self.candles) >= self.candle_cache_size:
+            self.update_step_size()
+
+    def update_candle_cache(self):
+        price = self.data.get_current_price()
         # maintain a running high and low to store the highest, lowest price in a day
-        self.high = max(self.high, candle.high)
-        self.low = min(self.low, candle.low)
+        self.high = max(self.high, price.high)
+        self.low = min(self.low, price.low)
 
-        # to check for open and close time in a daily candle, just mod them with 86_400_000
-        if candle.open_time % 86_400_000 == 0:
-            self.open = candle.open
-        # close time is minus by 1 millisecond, so we add 1 for it
-        if (int(candle.close_time) + 1) % 86_400_000 == 0:
-            self.close = candle.close
-            # edge case, no open candle
-            if self.open == 0:
-                self.open = self.close
-            # store what we have so far
-            self.daily_candles.append((self.open, self.high, self.low, self.close))
-            # reset variables
-            self.high, self.low = 0.0, 2 << 64
+        # to check for open and close time in a daily price, just mod them with 86_400_000
+        if price.open_time % 86_400_000 == 0:
+            self.open = price.open
+        # close time is smaller than next open time by 1 millisecond
+        if (int(price.close_time) + 1) % 86_400_000 == 0:
+            self.close = price.close
+            self.store_new_candle()
 
-        # calculate grid interval if we've gathered enough candles
-        if len(self.daily_candles) >= self.min_num_daily_candles:
-            prices = np.array(self.daily_candles[-self.min_num_daily_candles:])
-            # our formula for determine change per day is: (high - low) / open / step
-            changes = np.abs(prices[:, 1] - prices[:, 2]) / prices[:, 0]
-            # interval can't be too small, so we set 0.005 as minimum
-            self.interval = round(max(np.mean(changes) / self.step, 0.005), 3)
+    def store_new_candle(self):
+        # edge case, no open candle
+        if self.open == 0:
+            self.open = self.close
+
+        self.candles.append((self.open, self.high, self.low, self.close))
+        # reset variables, open & close aren't resetted because their value are determined by time
+        self.high, self.low = 0.0, np.inf
+
+    def update_step_size(self):
+        prices = np.array(self.candles[-self.candle_cache_size:])
+        # our formula for determine change per day is: (high - low) / open / step
+        changes = np.abs(prices[:, 1] - prices[:, 2]) / prices[:, 0]
+        # interval can't be too small, so we set 0.005 as minimum
+        self.interval = round(max(np.mean(changes) / self.interval, 0.005), 3)
 
     def place_grid_orders(self, orders: list[Order], side: str):
-        # check if any order is filled, if yes, refill orders depend on order type
         num_unfill_orders = self.grid_size - len(orders)
         assert num_unfill_orders >= 0
+        if num_unfill_orders > 0:
+            print(f"adding new {num_unfill_orders} {side} orders to the backend")
+
         # determine entry price for new order, use current price if grid is empty
         # else use the latest order's price as starting point
         price = self.data.get_close_price() if len(orders) == 0 else orders[-1].price
-        if num_unfill_orders > 0:
-            print(f"adding new {num_unfill_orders} {side} orders to the backend")
 
         for i in range(0, num_unfill_orders):
             price = utils.get_tp(price, self.interval, side)
