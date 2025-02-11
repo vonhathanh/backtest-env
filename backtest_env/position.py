46a10cc6ebf62b5eea7fb9d79b0d18d4937eed89
hanhvn <vonhathanh@gmail.com>
2025-02-11 17:19:49 +0700

    revamp position management: - Add LongPosition & ShortPosition - Position can be increase, decrease naturally

--- backtest_env/position.py
+++ backtest_env/position.py
@@ -1,10 +1,11 @@
 from backtest_env.order import Order
+from backtest_env.constants import LONG, SHORT, BUY, SELL
+from abc import ABC, abstractmethod
 
 
-class Position:
+class Position(ABC):
 
-    def __init__(self, side: str):
-        self.side = side
+    def __init__(self):
         self.quantity = 0.0
         self.avg_price = 0.0
 
@@ -12,13 +13,50 @@ class Position:
         self.quantity = 0.0
         self.avg_price = 0.0
 
-    # TODO: test get_pnl()
+    def decrease(self, delta: float):
+        assert self.quantity >= delta
+        self.quantity -= delta
+
+        if self.quantity == 0.0:
+            self.avg_price = 0.0
+
+    def increase(self, order: Order):
+        self.avg_price = ((self.quantity * self.avg_price + order.quantity * order.price) /
+                          (self.quantity + order.quantity))
+        self.quantity += order.quantity
+
+    @abstractmethod
+    def get_pnl(self, price: float) -> float:
+        pass
+
+    @abstractmethod
+    def update(self, order: Order):
+        pass
+
+
+class LongPosition(Position):
+
+    def __init__(self):
+        super().__init__()
+        self.side = LONG
+
+    # TODO: test pnl()
     def get_pnl(self, price: float) -> float:
         return self.quantity * (price - self.avg_price)
 
     # TODO: test update()
     def update(self, order: Order):
-        assert order.quantity > 0.0
-        self.avg_price = ((self.quantity * self.avg_price + order.quantity * order.price) /
-                          (self.quantity + order.quantity))
-        self.quantity += order.quantity
+        self.increase(order) if order.side == BUY else self.decrease(order.quantity)
+
+
+class ShortPosition(Position):
+
+    def __init__(self):
+        super().__init__()
+        self.side = SHORT
+
+    def get_pnl(self, price: float) -> float:
+        return self.quantity * (self.avg_price - price)
+
+    def update(self, order: Order):
+        self.increase(order) if order.side == SELL else self.decrease(order.quantity)
