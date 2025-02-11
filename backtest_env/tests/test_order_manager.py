cb92f2bf470b9d96ee764156d9ad1a4042b5eb06
hanhvn <vonhathanh@gmail.com>
2025-02-11 14:57:57 +0700

    test process_orders

--- backtest_env/tests/test_order_manager.py
+++ backtest_env/tests/test_order_manager.py
@@ -2,6 +2,7 @@ from unittest.mock import Mock
 
 from backtest_env.order import Order, OrderType
 from backtest_env.order_manager import OrderManager
+from backtest_env.price import Price
 from backtest_env.utils import get_random_string
 
 
@@ -29,3 +30,18 @@ class TestOrderManager:
         self.order_mgr.add_order(Order(1.0, "BNBUSDT", "SELL", OrderType.Market, "", 1.0, get_random_string()))
         long_orders, short_orders = self.order_mgr.split_orders_by_side()
         assert len(long_orders) == 1 and len(short_orders) == 2
+
+    def test_process_orders(self):
+        market_order = Order(100.0, "BNBUSDT", "BUY", OrderType.Market, "", 1.0, get_random_string())
+        limit_order = Order(120.0, "BNBUSDT", "BUY", OrderType.Limit, "", 1.0, get_random_string())
+
+        self.order_mgr.add_orders([market_order, limit_order])
+
+        # force price to return 100 so limit order not filled
+        self.order_mgr.data.get_current_price.return_value = Price(0, 100, 100, 100, 100, 0)
+
+        # process only market order so limit order will stay
+        self.order_mgr.process_orders()
+        orders = self.order_mgr.get_orders()
+        assert len(orders) == 1 and orders[0].type == OrderType.Limit
+
