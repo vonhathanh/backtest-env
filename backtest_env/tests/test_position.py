029e1e230c687bdd2d29eb97ccc620831aa2d23f
hanhvn <vonhathanh@gmail.com>
2025-02-14 17:36:30 +0700

    update tests according to new codes

--- backtest_env/tests/test_position.py
+++ backtest_env/tests/test_position.py
@@ -1,37 +1,37 @@
 import pytest
 
-from backtest_env.constants import LONG, SHORT, BUY, SELL
-from backtest_env.order import Order, OrderType
+from backtest_env.constants import BUY, SELL
 from backtest_env.position import LongPosition, ShortPosition
+from backtest_env.utils import create_short_order, create_long_order
 
 
 def test_increase_long_position():
     p = LongPosition()
 
     with pytest.raises(ValueError) as excinfo:
-        p.update(Order(500.0, "BNB", BUY, OrderType.Market, LONG, 0.0)) # quantity = 0
+        p.update(create_long_order(quantity=0.0))
     assert excinfo.type is ValueError
 
-    p.update(Order(500.0, "BNB", BUY, OrderType.Market, LONG, 1.0))
+    p.update(create_long_order(price=500.0, quantity=1.0))
     assert p.avg_price == 500.0 and p.quantity == 1.0
 
-    p.update(Order(400.0, "BNB", BUY, OrderType.Market, LONG, 1.0))
+    p.update(create_long_order(price=400.0, quantity=1.0))
     assert p.avg_price == 450.0 and p.quantity == 2.0
 
-    p.update(Order(650.0, "BNB", BUY, OrderType.Market, LONG, 2.0))
+    p.update(create_long_order(price=650.0, quantity=2.0))
     assert p.avg_price == 550.0 and p.quantity == 4.0
 
 
 def test_decrease_long_position():
     p = LongPosition()
 
-    p.update(Order(500.0, "BNB", BUY, OrderType.Market, LONG, 0.6))
+    p.update(create_long_order(quantity=0.6, price=500.0))
 
     with pytest.raises(ValueError) as excinfo:
-        p.update(Order(500.0, "BNB", SELL, OrderType.Market, LONG, 0.7)) # sell 0.7 while only have 0.6 bnb
+        p.update(create_long_order(side=SELL, quantity=0.7)) # sell 0.7 while only have 0.6 bnb
     assert excinfo.type is ValueError
 
-    p.update(Order(600.0, "BNB", SELL, OrderType.Market, LONG, 0.3))
+    p.update(create_long_order(side=SELL, quantity=0.3, price=600.0))
     assert p.quantity == 0.3 and p.avg_price == 500.0
 
 
@@ -39,36 +39,36 @@ def test_increase_short_position():
     p = ShortPosition()
 
     with pytest.raises(ValueError) as excinfo:
-        p.update(Order(500.0, "BNB", SELL, OrderType.Market, SHORT, 0.0))  # quantity = 0
+        p.update(create_short_order(quantity=0.0))  # quantity = 0
     assert excinfo.type is ValueError
 
-    p.update(Order(12.0, "BNB", SELL, OrderType.Market, SHORT, 1.0))
+    p.update(create_short_order(price=12.0))
     assert p.avg_price == 12.0 and p.quantity == 1.0
 
-    p.update(Order(7.0, "BNB", SELL, OrderType.Market, SHORT, 1.0))
+    p.update(create_short_order(price=7.0))
     assert p.avg_price == 9.5 and p.quantity == 2.0
 
-    p.update(Order(10.5, "BNB", SELL, OrderType.Market, SHORT, 2.0))
+    p.update(create_short_order(price=10.5, quantity=2.0))
     assert p.avg_price == 10.0 and p.quantity == 4.0
 
 
 def test_decrease_short_position():
     p = ShortPosition()
 
-    p.update(Order(500.0, "BNB", SELL, OrderType.Market, SHORT, 0.6))
+    p.update(create_short_order(price=500.0, quantity=0.6))
 
     with pytest.raises(ValueError) as excinfo:
-        p.update(Order(500.0, "BNB", BUY, OrderType.Market, SHORT, 0.7))  # buy 0.7 while only shorted 0.6 bnb
+        p.update(create_short_order(side=BUY, quantity=0.7))  # buy 0.7 while only shorted 0.6 bnb
     assert excinfo.type is ValueError
 
-    p.update(Order(600.0, "BNB", BUY, OrderType.Market, SHORT, 0.3))
+    p.update(create_short_order(price=600.0, quantity=0.3, side=BUY))
     assert p.quantity == 0.3 and p.avg_price == 500.0
 
 
 def test_get_long_pnl():
     p = LongPosition()
 
-    p.update(Order(500.0, "BNB", BUY, OrderType.Market, LONG, 1.0))
+    p.update(create_long_order(price=500.0))
     assert p.get_pnl(500.0) == 0.0
     assert p.get_pnl(555.3) == 55.3
     assert p.get_pnl(452.1) == -47.9
@@ -77,7 +77,7 @@ def test_get_long_pnl():
 def test_get_short_pnl():
     p = ShortPosition()
 
-    p.update(Order(500.0, "BNB", SELL, OrderType.Market, SHORT, 1.0))
+    p.update(create_short_order(price=500.0))
 
     assert p.get_pnl(500.0) == 0.0
     assert p.get_pnl(555.3) == -55.3
@@ -87,8 +87,8 @@ def test_get_short_pnl():
 def test_pnl_after_multiple_long_orders():
     p = LongPosition()
 
-    p.update(Order(500.0, "BNB", BUY, OrderType.Market, LONG, 1.0))
-    p.update(Order(560.0, "BNB", BUY, OrderType.Market, LONG, 2.0))
+    p.update(create_long_order(price=500.0))
+    p.update(create_long_order(price=560.0, quantity=2.0))
 
     assert p.get_pnl(600) == 180.0
 
@@ -96,7 +96,7 @@ def test_pnl_after_multiple_long_orders():
 def test_pnl_after_multiple_short_orders():
     p = ShortPosition()
 
-    p.update(Order(500.0, "BNB", SELL, OrderType.Market, SHORT, 1.0))
-    p.update(Order(560.0, "BNB", SELL, OrderType.Market, SHORT, 0.5))
+    p.update(create_short_order(price=500.0))
+    p.update(create_short_order(price=560.0, quantity=0.5))
 
     assert p.get_pnl(600.0) == -120.0
--- backtest_env/tests/test_position_manager.py
+++ backtest_env/tests/test_position_manager.py
@@ -1,8 +1,8 @@
 from unittest.mock import Mock
 
-from backtest_env.constants import BUY, LONG, SHORT, SELL
-from backtest_env.order import Order, OrderType
+
 from backtest_env.position_manager import PositionManager
+from backtest_env.utils import create_long_order, create_short_order
 
 
 class TestPositionManager:
@@ -13,14 +13,12 @@ class TestPositionManager:
         self.position_mgr = PositionManager(self.data, self.initial_balance)
 
     def test_fill_long_order(self):
-        order = Order(500.0, "BNB", BUY, OrderType.Market, LONG, 1.0)
-        self.position_mgr.fill(order)
+        self.position_mgr.fill(create_long_order(price=500.0))
 
         assert self.position_mgr.balance == self.initial_balance - 500.0
 
     def test_fill_short_order(self):
-        order = Order(250.0, "BTC", SELL, OrderType.Market, SHORT, 0.5)
-        self.position_mgr.fill(order)
+        self.position_mgr.fill(create_short_order(price=250.0, quantity=0.5))
 
         assert self.position_mgr.balance == self.initial_balance
         assert self.position_mgr.margin == 125
@@ -35,8 +33,7 @@ class TestPositionManager:
         assert self.position_mgr.get_number_of_active_positions() == 2
 
     def test_close_long_position(self):
-        order = Order(500.0, "BNB", BUY, OrderType.Market, LONG, 1.0)
-        self.position_mgr.fill(order)
+        self.position_mgr.fill(create_long_order(price=500.0))
 
         # close long position at price = 300
         self.data.get_open_price.return_value = 300.0
@@ -47,8 +44,7 @@ class TestPositionManager:
         assert self.position_mgr.balance == self.initial_balance - 200
 
     def test_close_short_position(self):
-        order = Order(500.0, "BNB", SELL, OrderType.Market, SHORT, 1.0)
-        self.position_mgr.fill(order)
+        self.position_mgr.fill(create_short_order(price=500.0))
 
         # close long position at price = 300
         self.data.get_open_price.return_value = 300.0
