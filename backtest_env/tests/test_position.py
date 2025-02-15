2d6a53b4fdc0ba72ee39c51744cb6dcec1ec28d5
hanhvn <vonhathanh@gmail.com>
2025-02-15 20:35:48 +0700

    replace all ValueError by AssertionError in tests

--- backtest_env/tests/test_position.py
+++ backtest_env/tests/test_position.py
@@ -8,9 +8,9 @@ from backtest_env.utils import create_short_order, create_long_order
 def test_increase_long_position():
     p = LongPosition()
 
-    with pytest.raises(ValueError) as excinfo:
+    with pytest.raises(AssertionError) as excinfo:
         p.update(create_long_order(quantity=0.0))
-    assert excinfo.type is ValueError
+    assert excinfo.type is AssertionError
 
     p.update(create_long_order(price=500.0, quantity=1.0))
     assert p.avg_price == 500.0 and p.quantity == 1.0
@@ -27,9 +27,9 @@ def test_decrease_long_position():
 
     p.update(create_long_order(quantity=0.6, price=500.0))
 
-    with pytest.raises(ValueError) as excinfo:
+    with pytest.raises(AssertionError) as excinfo:
         p.update(create_long_order(side=SELL, quantity=0.7)) # sell 0.7 while only have 0.6 bnb
-    assert excinfo.type is ValueError
+    assert excinfo.type is AssertionError
 
     p.update(create_long_order(side=SELL, quantity=0.3, price=600.0))
     assert p.quantity == 0.3 and p.avg_price == 500.0
@@ -38,9 +38,9 @@ def test_decrease_long_position():
 def test_increase_short_position():
     p = ShortPosition()
 
-    with pytest.raises(ValueError) as excinfo:
+    with pytest.raises(AssertionError) as excinfo:
         p.update(create_short_order(quantity=0.0))  # quantity = 0
-    assert excinfo.type is ValueError
+    assert excinfo.type is AssertionError
 
     p.update(create_short_order(price=12.0))
     assert p.avg_price == 12.0 and p.quantity == 1.0
@@ -57,9 +57,9 @@ def test_decrease_short_position():
 
     p.update(create_short_order(price=500.0, quantity=0.6))
 
-    with pytest.raises(ValueError) as excinfo:
+    with pytest.raises(AssertionError) as excinfo:
         p.update(create_short_order(side=BUY, quantity=0.7))  # buy 0.7 while only shorted 0.6 bnb
-    assert excinfo.type is ValueError
+    assert excinfo.type is AssertionError
 
     p.update(create_short_order(price=600.0, quantity=0.3, side=BUY))
     assert p.quantity == 0.3 and p.avg_price == 500.0
