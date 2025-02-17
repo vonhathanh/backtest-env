e30d56ea7c9036cf69b8063a1bece115ca7b67fb
hanhvn <vonhathanh@gmail.com>
2025-02-17 07:35:58 +0700

    script to test various scenarios

---
+++ backtest_env/scripts/pnl.py
@@ -0,0 +1,23 @@
+from backtest_env.order_manager import OrderManager
+from backtest_env.position_manager import PositionManager
+from backtest_env.utils import create_long_order, create_short_order
+from backtest_env.constants import SELL
+
+
+position_manager = PositionManager(None, 10000)
+order_manager = OrderManager(position_manager, None)
+
+first_order = create_long_order(price=100.0, quantity=1.0)
+
+order_manager.add_order(first_order)
+order_manager.process_orders()
+
+orders = []
+for i in range(1, 11):
+    orders.append(create_short_order(quantity=0.1, price=100 - i))
+order_manager.add_orders(orders)
+order_manager.process_orders()
+
+print(position_manager.get_pnl(50))
+
+
