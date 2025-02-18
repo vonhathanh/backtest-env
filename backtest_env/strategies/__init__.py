c90294582d521229394ad1e073a645503b78f87a
hanhvn <vonhathanh@gmail.com>
2025-02-18 07:47:31 +0700

    rename Base strategy (can't initialize it)

--- backtest_env/strategies/__init__.py
+++ backtest_env/strategies/__init__.py
@@ -3,7 +3,6 @@ from backtest_env.strategies.strategy import Strategy
 from backtest_env.strategies.trend_follower import TrendFollower
 
 STRATEGIES = {
-    "Base": Strategy,
     "Baseline": Baseline,
     "TrendFollower": TrendFollower,
 }
