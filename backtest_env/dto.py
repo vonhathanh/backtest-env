7e08c073b63f0155d24eab55dbaf6c9a5e60b191
hanhvn <vonhathanh@gmail.com>
2025-03-03 20:53:41 +0700

    add comment

--- backtest_env/dto.py
+++ backtest_env/dto.py
@@ -7,8 +7,8 @@ class Args(BaseModel):
     initialBalance: float
     symbol: str
     timeframe: str
-    startTime: str
-    endTime: Optional[str]
+    startTime: str  # YYYY-mm-dd format
+    endTime: Optional[str] # YYYY-mm-dd format
     strategy: str
 
 
