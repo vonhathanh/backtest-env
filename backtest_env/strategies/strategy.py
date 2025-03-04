d7628d963ac77ff52246022b9f4bdc7b3880046f
hanhvn <vonhathanh@gmail.com>
2025-03-04 19:37:18 +0700

    Strategy now posses websocket_client instead of inherit from it. Add pre_process() and post_process() functions

--- backtest_env/strategies/strategy.py
+++ backtest_env/strategies/strategy.py
@@ -6,6 +6,7 @@ from backtest_env.order_manager import OrderManager
 from backtest_env.position_manager import PositionManager
 from backtest_env.price import PriceDataSet
 from backtest_env.logger import logger
+from backtest_env.websocket_client import WebsocketClient
 
 T = TypeVar("T", bound="Strategy")
 
@@ -18,14 +19,28 @@ class Strategy(ABC):
         )
         self.position_manager = PositionManager(self.data, args.initialBalance)
         self.order_manager = OrderManager(self.position_manager, self.data)
+        self.ws_client: WebsocketClient = None
+        if args.isMonitor:
+            self.ws_client = WebsocketClient(self.__class__.__name__)
 
     def run(self):
         # main event loop: getting new candle stick and then process data based on update() logic
         # child class must override update() to specify their own trading logic
         while self.data.step():
+            self.pre_process()
             self.update()
+            self.post_process()
         self.cleanup()
         self.report()
+
+    def pre_process(self):
+        if self.ws_client is None:
+            return
+        self.ws_client.process_client_messages()
+        self.ws_client.emit(self.gather_status())
+
+    def post_process(self):
+        pass
 
     @abstractmethod
     def update(self):
@@ -33,6 +48,19 @@ class Strategy(ABC):
         # inspect new input data: prices, indicators,... from other sources
         # determine the next action: submit buy/sell order, cancel orders, close positions, ...
         pass
+
+    def gather_status(self):
+        price = self.data.get_current_price()
+        long, short = self.position_manager.get_positions()
+        orders = self.order_manager.get_all_orders()
+        order_history = self.order_manager.get_order_history()
+
+        return {
+            "price": price.json(),
+            "positions": [long.json(), short.json()],
+            "orders": [order.json() for order in orders],
+            "orderHistory": [order.json() for order in order_history],
+        }
 
     def cleanup(self):
         self.order_manager.cancel_all_orders()
