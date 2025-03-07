a042198555c542063fc5b8ac9aec6aba0553ed32
hanhvn <vonhathanh@gmail.com>
2025-03-08 06:11:17 +0700

    rename WEBSOCKET_URL to SOCKETIO_URL

--- backtest_env/constants.py
+++ backtest_env/constants.py
@@ -1,11 +1,12 @@
 import json
 import os
+from typing import Any
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(BASE_DIR, "../configs.json")
 
 with open(CONFIG_PATH, "r") as f:
-    config = json.load(f)
+    config: dict[str, Any] = json.load(f)
 
 # order sides
 BUY = "BUY"
@@ -16,4 +17,4 @@ LONG = "LONG"
 SHORT = "SHORT"
 
 DATA_DIR = os.path.join(BASE_DIR, "..", "data")
-WEBSOCKET_URL = config["websocket_uri"]
+SOCKETIO_URL = str(config["socketio_url"])
