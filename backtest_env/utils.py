10e6e40c6c5131ffc7e9f746a05e326eb3c72f06
hanhvn <vonhathanh@gmail.com>
2025-03-28 20:30:07 +0700

    move create_long_order & create_short_order to tests/utils.py

--- backtest_env/utils.py
+++ backtest_env/utils.py
@@ -5,30 +5,7 @@ import numpy as np
 from datetime import datetime
 from os.path import join
 
-from backtest_env.constants import BUY, SELL, LONG, SHORT, DATA_DIR
-from backtest_env.base.order import Order, OrderType
-
-
-def create_long_order(
-    order_type: OrderType = OrderType.Market,
-    side: str = BUY,
-    quantity=1.0,
-    symbol: str = "X",
-    price: float = 100.0,
-    position_side: str = LONG,
-):
-    return Order(order_type, side, quantity, symbol, price, position_side)
-
-
-def create_short_order(
-    order_type: OrderType = OrderType.Market,
-    side: str = SELL,
-    quantity=1.0,
-    symbol: str = "X",
-    price: float = 100.0,
-    position_side: str = SHORT,
-):
-    return Order(order_type, side, quantity, symbol, price, position_side)
+from backtest_env.constants import DATA_DIR
 
 
 def load_price_data(data_dir: str, symbol: str, tf: str, start: int, end: int = 0) -> np.ndarray:
@@ -64,11 +41,6 @@ def get_tp(price: float, percent: float, side: str) -> float:
     :return: entry price for take-profit to be triggered
     """
     return round(price * (1 + percent) if side == "BUY" else price * (1 - percent), 4)
-
-
-def to_position(side: str) -> str:
-    # convert order side to position side
-    return LONG if side == BUY else SHORT
 
 
 def convert_datetime_to_nanosecond(date: str, date_format: str = "%Y-%m-%d") -> int:
