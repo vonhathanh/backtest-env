e90095632efff82c4694b9068ecf182a53ab39e6
hanhvn <vonhathanh@gmail.com>
2025-11-29 12:02:36 +0700

    update seed data script

--- scripts/seed_data.py
+++ scripts/seed_data.py
@@ -7,3 +7,6 @@ DATA_DIR = os.path.join(BASE_DIR, "..", "data")
 # to seed data, we'll just simply download prepared-data from cloud storage to local repository
 # this script will never validate/ensure the correctness of the data it downloaded. That's another script's job
 snapshot_download("hanhvn/binance-data-collection", repo_type="dataset", local_dir=DATA_DIR)
+# remove all unneccessary files
+os.remove(os.path.join(DATA_DIR, ".cache"))
+os.remove(os.path.join(DATA_DIR, ".gitattributes"))
